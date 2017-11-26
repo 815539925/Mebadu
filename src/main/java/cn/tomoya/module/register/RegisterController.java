2d75e91d5a2fb86652c6055b768564ca150b96db
Administrator <Administrator@USER-20170423IE>
2017-11-26 23:32:22 +0800

    用户注册模块jsp,controller

---
+++ src/main/java/cn/tomoya/module/register/RegisterController.java
@@ -0,0 +1,12 @@
+package cn.tomoya.module.register;
+
+import cn.tomoya.common.BaseController;
+import cn.tomoya.utils.ext.route.ControllerBind;
+
+
+@ControllerBind(controllerKey = "/Register", viewPath = "WEB-INF/page")
+public class RegisterController extends BaseController{
+
+	
+
+}
